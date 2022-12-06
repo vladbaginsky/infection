1aae5b2f84f2dfddd02efa1d8a650b843a6446e2
vladbaginsky <vlad.baginsky0@gmail.com>
2022-12-06 10:23:07 +0300

    добавил самоизоляцию

--- main.py
+++ main.py
@@ -32,6 +32,10 @@ GAME_COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 WIDTH = 500
 HEIGHT = 400
 
+# если isolation only infected, то изолируем после появления симптомов,
+# если no isolation - никого никогда не изолируем,
+# если isolation == all - изолируем всех с самого начала
+isolation = "only infected"
 
 class Mob:
     def __init__(self, screen: pygame.Surface, x, y, color=BLACK, probab = 0.01):
@@ -43,15 +47,57 @@ class Mob:
         self.vy = 0
         self.ax = 0
         self.ay = 0
+        self.broun = 10
+        # определяет амплитуду броуновского движения
         self.color = BLACK
+        self.speed = 0.5
+        # скорость отвечающая за то, как далеко ходят люди, от 0 до 1
         self.inf = False
         self.screen = screen
         self.probab = probab        
         self.immu = False
         self.time = 0
-        self.timetogetwell = 10
+        self.timetogetwell = 15
         self.timeinffirst = 0
+        self.timetosymptoms = 5
+        # время до появления симптомов
         
+        #
+        self.isolated = False
+        # Если True, то изолируем его               
+    def isolate(self):
+        '''Если человек заболел, то изолируем его, когда появятся симптомы
+        (флажок isolation = "only infected"),
+        либо флажок all - изолируем всех
+        no isolation никого
+        Делаем оскорость(описанную выше) и 
+        амплитуду Броуновского движения нулевой,
+        оставляя то, что другие могут к нему приближаться
+        (не все соблюдают самоизоляцию)
+        Если выздоравливает отменяем
+        '''
+        if isolation == "only infected":
+            if per.inf == True:
+                if self.time > self.timetosymptoms:
+                    self.isolated = True
+                    self.broun = 30
+                    self.speed = 0
+            else:
+                self.isolated = False
+                self.broun = 10
+                self.speed = 0.5
+        if isolation == "all":
+            self.isolated = True
+            self.broun = 30
+            self.speed = 0
+                # можно было бы это возвращать к значениям,
+                # которые ввел пользователь
+                
+    #def die(self):
+      #  if self.inf = True:
+            
+                
+                
     def draw(self):
         if self.inf:
             
@@ -72,22 +118,25 @@ class Mob:
         
         
     def move(self, dt):
-        self.vx = randint(-10,10)/10 #+ randint(-1, 1)*0.1
-        self.vy = randint(-10,10)/10
+        
+        
+        self.vx = randint(-10,10)/self.broun + randint(-1, 1)*self.speed
+        self.vy = randint(-10,10)/self.broun + randint(-1, 1)*self.speed
         self.x += self.vx * dt
         self.y += self.vy * dt
         self.x = self.x % WIDTH
         self.y = self.y % HEIGHT
     
     def get_well(self):
+        '''
+        Функция выздоровления.
+        Если время, которое человек болеет больше времени, которое нужно,
+        чтобы поправиться, человек выздоравливает
+        '''
         if self.time >= self.timetogetwell:
-            randch(0.00001)
             self.inf = False
             self.immu = True
             
-        #self.vx += self.ax*dt
-        
-        #self.vy += self.ay*dt
 '''
         if (self.x > WIDTH - 5):
             self.x = WIDTH - 5
@@ -98,16 +147,12 @@ class Mob:
         if (self.y <  5):
             self.y =  5
 '''           
-    #def broun_move(self):
-        #self.ax = randint(-2, 2)
-        #self.ay = randint(-1, 1)
 
 finished = False
 
 pygame.init()
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-#screen = pygame.Surface((WIDTH,HEIGHT), pygame.SRCALPHA)
 
 clock = pygame.time.Clock()
 
@@ -123,13 +168,13 @@ for i in range(100):
     pers.append(a)
 
 pers[0].inf = True
-# pers[0].color = MAGENTA
+
 counter = 0
 
 # задание начального времени по умолчанию у всех
 for per in pers:
     per.timeinffirst = start_time
-print(start_time)
+
 
 
 screen.fill(WHITE)
@@ -142,7 +187,11 @@ while not finished:
         
         per.move(dt)
         per.draw()
-        per.get_well()
+        #per.die()
+        per.isolate()
+        
+        if per.inf == True:
+            per.get_well()
         
     
     # count для графика
@@ -150,22 +199,21 @@ while not finished:
     for per in pers:
         if per.inf:
             counter += 1
-        
+    
+    
     count_arr.append(counter)
     time_arr.append(int(time.time()-start_time))
     
     
     for per1 in pers:
+        per1.time = time.time()-per1.timeinffirst
         if per1.inf == True:
-            per1.time = time.time()-per1.timeinffirst
-            
             for per2 in pers:
-                if per2 == per1:
+                if per2.inf == True:
                     continue
                 if (per1.x-per2.x)**2 + (per1.y-per2.y)**2 < (per1.rinf)**2:
-                    if randch(per2.probab):
-                        per2.inf = not per2.immu
-                        per2.timeinffirst = int(time.time())
+                    per2.inf = not per2.immu
+                    per2.timeinffirst = int(time.time())
                         
     #print(pers[0].time)
         
@@ -179,7 +227,6 @@ while not finished:
             finished = True
 
 pygame.quit()
-
 
 # график
 
